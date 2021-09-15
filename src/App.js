a3ea79a76a49fcfe5a6c6863a6bd0b16f9599ace
ayuubb <ayubbkj2@gmail.com>
2021-09-15 21:52:47 +0700

    Setting API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [emojisData, setemojisData] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [eror, seteror] = useState(false);
+
+  useEffect(() => {
+    async function fetchEmojis() {
+      setloading(true);
+      try {
+        const res = await axios.get('https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28');
+        setemojisData(res.data);
+        setloading(false);
+      } catch (error) {
+        console.error(error);
+        seteror(false);
+        setloading(false);
+      }
+    }
+    fetchEmojis();
+  }, []);
+
+  console.log('loading ? ', loading);
+  console.log('error ? ', eror);
+  console.log('emojisData ? ', emojisData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hallo World</h1>
     </div>
   );
 }
